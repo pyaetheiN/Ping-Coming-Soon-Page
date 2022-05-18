dd3eb0c6e201cf8408c2edd39f89cafb0b328a3e
Pyae Thein <pyaethein246@gmail.com>
2022-05-18 21:11:05 +0630

    commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('.form'),
       email = document.querySelector('.form__input'),
       errorMsg = document.querySelector('.form__input--error'),
-      pattern = /^([a-z0-9.]){2,20}@{1}([a-z0-9-]){2,20}\.{1}([a-z0-9]){2,3}(\.([a-z0-9]){2,3})?$/;
+      pattern = /^([a-zA-Z0-9.]){2,20}@{1}([a-z0-9-]){2,20}\.{1}([a-z0-9]){2,3}(\.([a-z0-9]){2,3})?$/;
 
 // subdomain and domain ex. hello@newsletter.company.com
 
